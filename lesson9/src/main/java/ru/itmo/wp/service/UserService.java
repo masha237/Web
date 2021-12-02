2ffcc2af9da2b40c873b7d531476b4496d25b07a
masha237 <zhogowamasha@gmail.com>
2021-12-02 04:52:14 +0300

    add

---
+++ lesson9/src/main/java/ru/itmo/wp/service/UserService.java
@@ -0,0 +1,69 @@
+package ru.itmo.wp.service;
+
+import org.springframework.stereotype.Service;
+import ru.itmo.wp.domain.Post;
+import ru.itmo.wp.domain.Role;
+import ru.itmo.wp.domain.Tag;
+import ru.itmo.wp.domain.User;
+import ru.itmo.wp.form.UserCredentials;
+import ru.itmo.wp.repository.RoleRepository;
+import ru.itmo.wp.repository.TagRepository;
+import ru.itmo.wp.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    /** @noinspection FieldCanBeLocal, unused */
+    private final RoleRepository roleRepository;
+    private final TagRepository tagRepository;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, TagRepository tagRepository) {
+        this.userRepository = userRepository;
+
+        this.roleRepository = roleRepository;
+        this.tagRepository = tagRepository;
+        for (Role.Name name : Role.Name.values()) {
+            if (roleRepository.countByName(name) == 0) {
+                roleRepository.save(new Role(name));
+            }
+        }
+
+        for (Tag.Name name : Tag.Name.values()) {
+            if (tagRepository.countByName(name) == 0) {
+                tagRepository.save(new Tag(name));
+            }
+        }
+    }
+
+    public User register(UserCredentials userCredentials) {
+        User user = new User();
+        user.setLogin(userCredentials.getLogin());
+        userRepository.save(user);
+        userRepository.updatePasswordSha(user.getId(), userCredentials.getLogin(), userCredentials.getPassword());
+        return user;
+    }
+
+    public boolean isLoginVacant(String login) {
+        return userRepository.countByLogin(login) == 0;
+    }
+
+    public User findByLoginAndPassword(String login, String password) {
+        return login == null || password == null ? null : userRepository.findByLoginAndPassword(login, password);
+    }
+
+    public User findById(Long id) {
+        return id == null ? null : userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAllByOrderByIdDesc();
+    }
+
+    public void writePost(User user, Post post) {
+        user.addPost(post);
+        userRepository.save(user);
+    }
+}
