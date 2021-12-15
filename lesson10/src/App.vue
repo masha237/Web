ca59befc76cc490a8ab5311e333cf0233021965b
masha237 <zhogowamasha@gmail.com>
2021-12-15 19:36:24 +0300

    kek

---
+++ lesson10/src/App.vue
@@ -0,0 +1,121 @@
+<template>
+    <div id="app">
+        <Header :user="users[userId]"/>
+        <Middle :posts="posts" :users="users" :comments="comments"/>
+
+        <Footer :postsCount="getLength(posts)" :usersCount="getLength(users)"/>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Middle from "./components/Middle";
+import Footer from "./components/Footer";
+
+export default {
+    name: 'App',
+    components: {
+        Footer,
+        Middle,
+        Header
+    },
+    data: function () {
+        return this.$root.$data;
+    },
+    provide() {
+        return {
+            getLength: this.getLength,
+        }
+    },
+    methods: {
+        getLength(array) {
+            return Object.keys(array).length;
+        }
+    },
+    beforeCreate() {
+        this.$root.$on("onEnter", (login, password) => {
+            if (password === "") {
+                this.$root.$emit("onEnterValidationError", "Password is required");
+                return;
+            }
+            const users = Object.values(this.users).filter(u => u.login === login);
+            if (users.length === 0) {
+                this.$root.$emit("onEnterValidationError", "No such user");
+            } else {
+                this.userId = users[0].id;
+                this.$root.$emit("onChangePage", "Index");
+            }
+        });
+
+        this.$root.$on("onLogout", () => this.userId = null);
+
+        this.$root.$on("onWritePost", (title, text) => {
+            if (this.userId) {
+                if (!title || title.length < 5) {
+                    this.$root.$emit("onWritePostValidationError", "Title is too short");
+                } else if (!text || text.length < 10) {
+                    this.$root.$emit("onWritePostValidationError", "Text is too short");
+                } else {
+                    const id = Math.max(...Object.keys(this.posts)) + 1;
+                    this.$root.$set(this.posts, id, {
+                        id, title, text, userId: this.userId
+                    });
+                }
+            } else {
+                this.$root.$emit("onWritePostValidationError", "No access");
+            }
+        });
+
+        this.$root.$on("onRegister", (login, name) => {
+            if (!login || login.length < 3 || login.length > 16) {
+                this.$root.$emit("onRegisterValidationError", "Login's should contain 3 to 16 characters");
+            } else if (!name || name.length < 1 || name.length > 32) {
+                this.$root.$emit("onRegisterValidationError", "Name's should contain 1 to 32 characters");
+            } else if (!/^[a-z]+$/i.test(login)) {
+                this.$root.$emit("onRegisterValidationError", "Login should contain only Latin letters");
+            } else if (!/^[a-z][a-z ]+$/i.test(name)) {
+                this.$root.$emit("onRegisterValidationError", "Name should contain only Latin letters and spaces. Name should begin from letters");
+            } else {
+                const users = Object.values(this.users).filter(u => u.login === login);
+                if (users.length !== 0) {
+                    this.$root.$emit("onRegisterValidationError", "Login is already used");
+                    return;
+                }
+                const id = Math.max(...Object.keys(this.users).map((id) => parseInt(id))) + 1;
+                this.$root.$set(this.users, id, {
+                    id, login, name, admin: false
+                });
+                this.$root.$emit("onChangePage", "Enter");
+            }
+
+        });
+
+        this.$root.$on("onEditPost", (id, text) => {
+            if (this.userId) {
+                if (!id) {
+                    this.$root.$emit("onEditPostValidationError", "ID is invalid");
+                } else if (!text || text.length < 10) {
+                    this.$root.$emit("onEditPostValidationError", "Text is too short");
+                } else {
+                    let posts = Object.values(this.posts).filter(p => p.id === parseInt(id));
+                    if (posts.length) {
+                        posts.forEach((item) => {
+                            item.text = text;
+                        });
+                    } else {
+                        this.$root.$emit("onEditPostValidationError", "No such post");
+                    }
+                }
+            } else {
+                this.$root.$emit("onEditPostValidationError", "No access");
+            }
+        });
+    }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ lesson10/src/components/page/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <FullPost v-for="post in posts" :post="post" :comments="viewComment(post)" :users="users" :viewComments="false" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+import FullPost from "@/components/page/Viewers/FullPost";
+export default {
+    name: "Index",
+    props: ["posts", "comments", "users"],
+    components: {
+        FullPost
+    },
+    methods: {
+        viewComment: function (post) {
+            return Object.values(this.comments).filter(c => c.postId === post.id).sort((a, b) => a.id - b.id);
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
