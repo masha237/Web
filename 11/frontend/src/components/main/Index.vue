8ea9540e7e027ecc0dae7cac0e5205a5a0e06ca7
masha237 <zhogowamasha@gmail.com>
2021-12-26 13:38:41 +0300

    .gitignore is now working

---
+++ 11/frontend/src/components/main/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <FullPost v-for="post in posts" :post="post" :users="users" :viewComments="false" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+import FullPost from "./Viewers/FullPost";
+export default {
+    name: "Index",
+    props: ["posts", "users"],
+    inject: ["viewPost"],
+    components: {
+        FullPost
+    }
+}
+</script>
+
+<style scoped>
+</style>
